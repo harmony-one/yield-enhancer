a94090c862822d15315959454de3add6d48a348e
artemkolodko <temakolodko@gmail.com>
2025-01-15 08:29:30 +0000

    Update contract addresses

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,7 +1,7 @@
 export const appConfig = {
   walletConnectProjectId: 'd9582a28e2633cc50849bab7aa9ad668',
   sDaiTokenAddress: '0x00c853b9abD00Ebb44E3d0510203788A2E8A1206',
-  stakingVaultAddress: '0x6a3eEf794DA5166a4C3824D3d06cAAf3BD5F25B7',
-  stakingVaultLaunchBlock: 67783822n,
+  stakingVaultAddress: '0x4A842f722B36cE3Bf853Db32E7Ff16a44369230b',
+  stakingVaultLaunchBlock: 68108906n,
   apiUrl: 'https://hmy-erc4626-reward-dist.fly.dev'
 }
